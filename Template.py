4c66c64f7bc858dced2dee14727ff5ea8c8ef2f2
Matías Rodríguez <matata@MacBook-Pro-de-Matias.local>
2019-12-08 00:44:50 -0300

    get track by name

--- Template.py
+++ Template.py
@@ -11,15 +11,20 @@ class Template(UserActionsBase):
 
     def create_actions(self):
         self.add_global_action('tpl', self.entry_point_handler)
-        self.add_clip_action('get_clip', self.set_clip)
+        self.add_clip_action('get_clip', self.get_clip)
+        self.add_track_action('get_track', self.get_track)
 
     def entry_point_handler(self, _, args):
         trigger = self.canonical_parent.clyphx_pro_component.trigger_action_list
         self.tpl.dispatch(args, trigger)
 
-    def set_clip(self, action_def, args):
+    def get_clip(self, action_def, args):
         clip = action_def['clip']
-        self.tpl.set_clip(clip)
+        self.tpl.set_target_clip(clip)
+
+    def get_track(self, action_def, args):
+        track = action_def['track']
+        self.tpl.set_target_track(track)
 
     def on_selected_track_changed(self):
         self.tpl.set_selected_track(self.song().view.selected_track)
--- template/TemplateBase.py
+++ template/TemplateBase.py
@@ -2,8 +2,8 @@ from utils.log_utils import dumpobj,str2bool
 
 class TemplateBase:
     
-    _prev_track_helper = []
-    _selected_clip = None
+    _target_clip = None
+    _target_track = None
 
     def _do_init(self, live):
         self.log = live.canonical_parent.log_message
@@ -12,41 +12,38 @@ class TemplateBase:
 
     def dump(self):
         self.log('dump action start')
-        self._get_clip()
+        self._get_track()
         # self.trigger('"Production"/SEL')
         
     #track utils
-    def set_selected_track(self, track):
-        if len(self._prev_track_helper) < 2: 
-            self._prev_track_helper.append(track)
+    def set_target_track(self, track):
+        if track:
+            self._target_track = track
         else:
-            del self._prev_track_helper[0]
-            self._prev_track_helper.append(track)
-
-    def _previously_selected_track(self):
-        self.log('previous_track name: %s' % self._prev_track_helper[0:1][0].name )
-        self.log_obj(self._prev_track_helper[0:1][0], False)
-        #   return self._prev_track[0:1]
-
-    def _selected_track(self):
-        return self.live.song().view.selected_track
+            self._target_track = None
+    
+    def _get_track(self, track_name = False):
+        target = 'SEL' if not track_name else '"%s"' % track_name
+        self.trigger('%s/get_track' % target)
+        self.log('selected track name: %s' % getattr(self._target_track, "name", "No target track"))
+        return self._target_track
 
 
     #clip utils
-    def set_clip(self, clip):
+    def set_target_clip(self, clip):
         if clip:
-            self._selected_clip = clip
+            self._target_clip = clip
         else:
-            self._selected_clip = None
+            self._target_clip = None
 
 
     def _get_detail_clip(self):
         return self._get_clip('DETAIL')
     
     def _get_clip(self, target = 'SEL'):
-        self.trigger("user_clip(%s) get_clip" % target) #ojo con esto, puede que trigger este definido porque se define en dispatch
-        self.log('selected clip name: %s' % getattr(self._selected_clip, "name", "No clip selected"))
-        return self._selected_clip
+        self.trigger("user_clip(%s) get_clip" % target)
+        self.log('selected clip name: %s' % getattr(self._target_clip, "name", "No target clip"))
+        return self._target_clip
 
     #general
     def dispatch(self, args, trigger):
