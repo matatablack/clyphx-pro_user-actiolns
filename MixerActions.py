8570b7d4510950013328452d2711d29ca0985055
Matias Rodriguez <matiasm.rodriguez@hotmail.com>
2022-02-02 03:20:32 -0300

    UNASSIGN , unbind, mappings.

--- MixerActions.py
+++ MixerActions.py
@@ -1,7 +1,9 @@
+
 from ClyphX_Pro.clyphx_pro.UserActionsBase import UserActionsBase
 from utils.getters import selected_track 
 from utils.log_utils import dumpobj
 import re
+import time
 
 class MixerActions(UserActionsBase):
 
@@ -10,6 +12,8 @@ class MixerActions(UserActionsBase):
         self.add_global_action('mixer_monitor', self.monitor)
         self.add_global_action('daw_monitor', self.daw_monitor)
         self.add_track_action('mixer_assign', self.assign)
+        self.add_track_action('mixer_unassign', self.unassign)
+        self.add_track_action('mixer_unbind', self.unbind_all)
 
 
     dumpobj = dumpobj
@@ -31,7 +35,20 @@ class MixerActions(UserActionsBase):
         "19/20":"[19/20] Space", 
         "21/22":"[21/22] Timefactor",
         "23/24":"[23/24] Deluge", 
-        "25/26": "[25/26] something"
+        "25/26": "[25/26] Pitchfactor"
+    }
+
+    group_name_by_channel = { 
+        "1": "KICK 1 [BUS]",
+        "2": "BASS 2 [BUS]",
+        "3/4": "DRUMS 3/4 [BUS]",
+        "5/6": "HITS 5/6 [BUS]",
+        "7/8": "MUSIC 7/8 [BUS]",
+        "9/10": "VOX 9/10 [BUS]",
+        "11/12": "ATMOSPHERE 11/12 [BUS]",
+        "13/14": "FX 13/14 [BUS]",
+        "15/16": "ALIENS 15/16 [BUS]",
+        "MIDI_1": "UP MIDI_1 [BUS]"
     }
 
     def add(self, action_def, args):
@@ -39,14 +56,7 @@ class MixerActions(UserActionsBase):
             args = args.split()
             channel = args[0]
             self.canonical_parent.log_message('-----ADD-----')
-            # self.canonical_parent.trigger_action_list('[%s] recall' % ident)
-            # self.canonical_parent.log_message(dumpobj(self.song().view.selected_track))
-            tracks = self.get_tracks_if_name_contains(channel)
-            # self.canonical_parent.log_message(channel)
-            # self.canonical_parent.clyphx_pro_component.trigger_action_list('msg "testing!!!! %s"' % tracks[0].name)
-
-            output_track = self.get_output_track_by_channel(channel)
-            
+            output_track = self.get_output_track_by_channel(channel)            
             dictionary = { 
                 'track_name': self.track_name_by_channel[channel],
                 'track_channel': channel,
@@ -67,19 +77,26 @@ class MixerActions(UserActionsBase):
                 SEL/ARM;
             '''.format(**dictionary)
 
-            #SEL/OUT "{output}";
-
             self.canonical_parent.clyphx_pro_component.trigger_action_list(actions)
 
-            # selected_track = self.song().view.selected_track
-            #put into arrange group?
 
             
         except BaseException as e:
             self.canonical_parent.log_message('ERROR: ' + str(e))
 
+
+
     """
-        to do -> if not mixer group and 
+    ASSIGN
+        selected channel
+        -> add external audio effect with audio to target mixer channel
+        -> add "assigned" state on channel name 
+        -> INSAUDIO o add track and set input from target mixer channel (return)
+            name equals previous track name + resample
+            monitor auto
+            arm on
+            bind fader_${target_channel} to sel/vol
+
     """
     def assign(self, action_def, args):
         try:
@@ -88,37 +105,104 @@ class MixerActions(UserActionsBase):
             track_idx = list(self.song().tracks).index(track) + 1
             args = args.split()
             channel = args[0]
-            output_track = None
-            if channel == 'origin':
-                origin_channel = self.get_track_channel(track)
-                # self.canonical_parent.log_message("origin channel -> %s" % origin_channel)
-                output_track = self.get_output_track_by_channel(origin_channel).name if origin_channel else None
+            is_midi = args[0] == "midi"
 
-            if channel == 'prev':
-                prev_out = self.get_track_prev_channel(track)
-                # self.canonical_parent.log_message("prev out -> %s" % prev_out)
-                output_track = prev_out if prev_out else None
+            if is_midi: 
+                fader_num = int(args[1])
+                assigned_to = '[-> midi %s]' % fader_num
+                tracks = self.get_tracks_if_name_contains(assigned_to)
+                if len(tracks) > 0: 
+                    self.canonical_parent.log_message('Channel Already Assigned to %s' % tracks[0].name)
+                    return
+                
+                dictionary = { 
+                    'track_idx': track_idx,
+                    'track_name': ("%s %s" % (track.name.split(assigned_to)[0], assigned_to)),
+                    'fader_num': fader_num,
+                    'knob_1_index' : fader_num,
+                    'knob_2_index' : fader_num + 4,
+                    'knob_3_index' : fader_num + 8
+                }
+                actions = '''
+                    SEL/NAME "{track_name}";
+                    BIND FADER_MIDI_{fader_num} "{track_name}"/VOL;
+                    BIND KNOB_{knob_1_index} "{track_name}"/PAN;
+                    BIND KNOB_{knob_2_index} "{track_name}"/SEND B;
+                    BIND KNOB_{knob_3_index} "{track_name}"/SEND A;
+                '''.format(**dictionary)
+                self.canonical_parent.log_message(actions)
+                self.canonical_parent.clyphx_pro_component.trigger_action_list(actions)
+            else: 
+                assigned_to = '[-> %s]' % channel
+                tracks = self.get_tracks_if_name_contains(assigned_to)
+                if len(tracks) > 0: 
+                    self.canonical_parent.log_message('Channel Already Assigned to %s' % tracks[0].name)
+                    return
 
-            if channel == 'master':
-                output_track = "Master"
-
-
-            current_out = track.output_routing_type.display_name.encode('utf8')
-            prev_out = current_out
-
-            self.canonical_parent.log_message("current out -> %s" % current_out)
-            self.canonical_parent.log_message("prev out -> %s" % prev_out)
+                dictionary = { 
+                    'track_idx': track_idx,
+                    'track_name': ("%s %s" % (track.name.split(assigned_to)[0], assigned_to)),
+                    'channel': channel,
+                    'dev_name': ("ToMixer%s.adv" % (channel.split('/')[0])), 
+                    'channel_first': channel.split('/')[0],
+                    'bus_channel': self.group_name_by_channel[channel],
+                    # 'rec_channel': self.group_name_by_channel[channel],
+                }       
+                actions = '''
+                    SEL/NAME "{track_name}";
+                    SEL/MUTE;
+                    LOADUSER "{dev_name}";
+                    WAIT 3;           
+                    INSAUDIO;
+                    WAIT 3;  
+                    SEL/INSUB "Post FX";         
+                    BIND FADER_{channel_first} "From {track_name}"/VOL;
+                    "{bus_channel}"/DEV("ToMixer") OFF;
+                    "{bus_channel}"/DEV("ToMixer") MON IN;
+                '''.format(**dictionary)
+                self.canonical_parent.log_message(actions)
+                self.canonical_parent.clyphx_pro_component.trigger_action_list(actions)
             
-            dictionary = { 
-                'track_idx': track_idx,
-                'output_track': output_track if output_track != None else self.get_output_track_by_channel(channel).name,
-                'track_name': ("%s --%s--" % (track.name.split(' --')[0], prev_out))
-            }
+        except BaseException as e:
+            self.canonical_parent.log_message('ERROR: ' + str(e))
 
-            actions = '''
-                {track_idx}/OUT "{output_track}";
-                {track_idx}/NAME "{track_name}";
-            '''.format(**dictionary)
+
+
+
+    def unassign(self, action_def, args):
+        try:
+            self.canonical_parent.log_message('-----UNNNNNNASSIGN-----')
+            track = action_def['track']
+            
+            args = args.split()
+            # channel = args[0]
+
+            assigned_prefix = '[->'
+            tracks = self.get_tracks_if_name_contains(assigned_prefix)
+            for track in tracks:
+                search = re.search(r'\[->(.*?)\]', track.name)
+                channel = search.group(1) if search else None 
+                channel_first = channel.split('/')[0]
+                to_mixer_dev_name = 'ToMixer%s' % channel_first
+                
+                track_idx = list(self.song().tracks).index(track) + 1
+
+                dictionary = { 
+                    'track_idx': track_idx,
+                    'track_name': track.name.split(assigned_prefix)[0],
+                    'dev_name': to_mixer_dev_name,
+                    'channel': channel,
+                    'channel_first': channel_first,
+                }
+
+                actions = '''
+                    {track_idx}/NAME "{track_name}";
+                    {track_idx}/DEV("{dev_name}") DEL;
+                    BIND FADER_{channel_first} NONE;
+                    "KICK 1 [BUS]", "BASS 2 [BUS]", "DRUMS 3/4 [BUS]", "HITS 5/6 [BUS]", "MUSIC 7/8 [BUS]", "VOX 9/10 [BUS]", "ATMOSPHERE 11/12 [BUS]", "FX 13/14 [BUS]", "ALIENS 15/16 [BUS]"/DEV("ToMixer") ON;
+                    "KICK 1 [BUS]", "BASS 2 [BUS]", "DRUMS 3/4 [BUS]", "HITS 5/6 [BUS]", "MUSIC 7/8 [BUS]", "VOX 9/10 [BUS]", "ATMOSPHERE 11/12 [BUS]", "FX 13/14 [BUS]", "ALIENS 15/16 [BUS]"/MON AUTO
+                '''.format(**dictionary)
+
 
             self.canonical_parent.log_message(actions)
 
@@ -126,6 +210,60 @@ class MixerActions(UserActionsBase):
             
         except BaseException as e:
             self.canonical_parent.log_message('ERROR: ' + str(e))
+
+    def unbind_all(self, action_def, args):
+        try:
+            self.canonical_parent.log_message('-----UNBIND-----')
+            track = action_def['track']
+            track_idx = list(self.song().tracks).index(track) + 1
+            dictionary = { 
+                'track_idx': track_idx,
+            }   
+            actions = '''                  
+                    BIND KNOB_1 "Bass [rec]"/PAN;
+                    BIND KNOB_2 "Drums [rec]"/PAN;
+                    BIND KNOB_3 "Hits [rec]"/PAN;
+                    BIND KNOB_4 SEL/SEND A;
+                    BIND KNOB_5 "Music [rec]"/PAN;
+                    BIND KNOB_6 "Vox [rec]"/PAN;
+                    BIND KNOB_7 "Atmosphere [rec]"/PAN;
+                    BIND KNOB_8 SEL/SEND B;
+                    BIND KNOB_9 "FX [rec]"/PAN;
+                    BIND KNOB_10 "Aliens [rec]"/PAN;
+                    BIND KNOB_11 "Up [rec]"/PAN;
+                    BIND KNOB_12 SEL/PAN;
+
+                    BIND FADER_1 "Kick [rec]"/VOL
+                    BIND FADER_2 "Bass [rec]"/VOL
+                    BIND FADER_3 "Drums [rec]"/VOL
+                    BIND FADER_4 NONE
+                    BIND FADER_5 "Hits [rec]"/VOL
+                    BIND FADER_6 NONE
+                    BIND FADER_7 "Music [rec]"/VOL
+                    BIND FADER_8 NONE
+                    BIND FADER_9 "Vox [rec]"/VOL
+                    BIND FADER_10 NONE
+                    BIND FADER_11 "Atmosphere [rec]"/VOL
+                    BIND FADER_12 NONE
+                    BIND FADER_13 "FX [rec]"/VOL
+                    BIND FADER_14 NONE
+                    BIND FADER_15 "Aliens [rec]"/VOL
+                    BIND FADER_16 NONE
+
+                    BIND FADER_MIDI_1 "Up [rec]"/VOL
+                    BIND FADER_MIDI_2 "REFERENCE [rec]"/VOL
+                    BIND FADER_MIDI_3 NONE
+                    BIND FADER_MIDI_4 SEL/VOL
+            '''.format(**dictionary)
+
+            self.canonical_parent.log_message(actions)
+            self.canonical_parent.clyphx_pro_component.trigger_action_list(actions)
+            
+        except BaseException as e:
+            self.canonical_parent.log_message('ERROR: ' + str(e))
+
+
+
 
     def monitor(self, action_def, args):
         try:
